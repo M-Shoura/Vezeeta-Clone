6cf90a87d3833c8a318b56e4ba43869e5b5d5dd5
Mahmoud Shoura <1m.shoura1@gmail.com>
2026-05-07 23:50:43 +0300

    Domain - Adding Enums

---
+++ Vezeeta/Domain/Enums/AppointmentStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Enums
+{
+    public enum AppointmentStatus : byte
+    {
+        Pending,
+        Confirmed,
+        Completed,
+        Cancelled
+    }
+}
---
+++ Vezeeta/Domain/Enums/DaysOfWeek.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Enums
+{
+    public enum DaysOfWeek : byte
+    {
+        Saturday,
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday
+    }
+}
