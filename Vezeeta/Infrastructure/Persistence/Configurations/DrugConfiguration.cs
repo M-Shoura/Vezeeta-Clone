52bf4ee1738d0237a4b924a11a06cb0ae362d719
moateff <mohamed.atef.yousef46@gmail.com>
2026-05-09 00:32:49 +0300

    Infra - Adding entities config.

---
+++ Vezeeta/Infrastructure/Persistence/Configurations/DrugConfiguration.cs
@@ -0,0 +1,51 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infranstructure.Persistence.Configurations
+{
+    public class DrugConfiguration : IEntityTypeConfiguration<Drug>
+    {
+        public void Configure(EntityTypeBuilder<Drug> builder)
+        {
+            // Table Name
+            builder.ToTable("Drugs");
+
+            // Primary Key
+            builder.HasKey(d => d.Id);
+
+            // Properties
+            builder.Property(d => d.Name)
+                   .IsRequired()
+                   .HasMaxLength(200);
+
+            builder.Property(d => d.Description)
+                   .HasMaxLength(1000);
+
+            builder.Property(d => d.Price)
+                   .HasColumnType("decimal(18,2)")
+                   .IsRequired();
+
+            builder.Property(d => d.GenericName)
+                   .HasMaxLength(200);
+
+            builder.Property(d => d.Manufacturer)
+                   .HasMaxLength(200);
+
+            builder.Property(d => d.Strength)
+                   .HasMaxLength(100);
+
+            builder.Property(d => d.SideEffects)
+                   .HasMaxLength(2000);
+
+            // Indexes
+            builder.HasIndex(d => d.Name);
+
+            // Relationships
+            builder.HasMany(d => d.PrescriptionItems)
+                   .WithOne(pi => pi.Drug)
+                   .HasForeignKey(pi => pi.DrugId)
+                   .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
