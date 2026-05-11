769474fbc9aee046548535829050713c225ff1d0
Youssef Wagih <youssefwagih212@gmail.com>
2026-05-11 23:15:09 +0300

    add: unit of work

---
+++ Vezeeta/Application/Interfaces/Repositories/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Interfaces.Repositories
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IGenericRepository<T> Repository<T>() where T : class;
+        int SaveChanges();
+        Task<int> SaveChangesAsync();
+    }
+}
---
+++ Vezeeta/Infrastructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,48 @@
+﻿using Application.Interfaces.Repositories;
+using Infranstructure.Persistence.Data;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ConcurrentDictionary<Type, object> _repositories = new();
+
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IGenericRepository<T> Repository<T>() where T : class
+        {
+            var type = typeof(T);
+            if (_repositories.TryGetValue(type, out var repository))
+            {
+                return (IGenericRepository<T>)repository;
+            }
+
+            var newRepository = new GenericRepository<T>(_context);
+            _repositories.TryAdd(type, newRepository);
+            return newRepository;
+        }
+
+        public int SaveChanges()
+        {
+            return _context.SaveChanges();
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync();
+             
+        }
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+    }
+}
