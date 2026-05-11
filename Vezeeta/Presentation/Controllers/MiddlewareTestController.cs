6796df1ee7a61aed87567fac880d56cb6266d136
Youssef Wagih <youssefwagih212@gmail.com>
2026-05-11 23:26:36 +0300

    add middleware extensions

---
+++ Vezeeta/Presentation/Controllers/MiddlewareTestController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers
+{
+    public class MiddlewareTestController : Controller
+    {
+        [HttpGet("ok")]
+        public IActionResult OK()
+        {
+            return Json(new { message = "Middleware pipeline is working." });
+        }
+
+        [HttpGet("throw")]
+        public IActionResult Throw()
+        {
+            throw new InvalidOperationException("Test exception from middleware endpoint.");
+        }
+    }
+}
---
+++ Vezeeta/Presentation/Extensions/MiddlewareExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Presentation.Middlewares;
+
+namespace Presentation.Extensions
+{
+    public static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder UseSharedMiddleware(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<GlobalExceptionHandlingMiddleware>();
+            app.UseMiddleware<RequestLoggingMiddleware>();
+
+            return app;
+        }
+    }
+}
