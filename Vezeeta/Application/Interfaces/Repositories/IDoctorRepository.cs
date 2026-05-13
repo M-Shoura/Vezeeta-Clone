90345c4c1616df7bf888d2ad0fb16027ab94dd9c
Mahmoud Shoura <1m.shoura1@gmail.com>
2026-05-13 16:17:18 +0300

    initial DR

---
+++ Vezeeta/Application/Interfaces/Repositories/IDoctorRepository.cs
@@ -0,0 +1,20 @@
+﻿using Application.DTOs.Doctors;
+
+namespace Application.Interfaces.Repositories
+{
+    public interface IDoctorRepository
+    {
+        Task<IEnumerable<DoctorDto>>
+            GetAllDoctorsAsync();
+
+        Task<DoctorDto?>
+            GetDoctorByIdAsync(string doctorId);
+
+        Task<IEnumerable<DoctorDto>>
+            GetAvailableDoctorsAsync();
+
+        Task<IEnumerable<DoctorDto>>
+            GetDoctorsBySpecializationAsync(
+                string specialization);
+    }
+}
