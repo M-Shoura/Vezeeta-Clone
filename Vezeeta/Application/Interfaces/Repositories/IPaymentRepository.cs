08887485b1f9de36be5b0ebdde731091dbdad3aa
Youssef Wagih <youssefwagih212@gmail.com>
2026-05-14 05:59:15 +0300

    payment: use generic repo for crud and custom repo for others

--- Vezeeta/Application/Interfaces/Repositories/IPaymentRepository.cs
+++ Vezeeta/Application/Interfaces/Repositories/IPaymentRepository.cs
@@ -1,4 +1,5 @@
 using Application.DTOs.Payments;
+using Domain.Entities;
 using Domain.Enums;
 
 namespace Application.Interfaces.Repositories
@@ -6,6 +7,7 @@ namespace Application.Interfaces.Repositories
     public interface IPaymentRepository
     {
         Task<PaymentDto?> GetPaymentByIdAsync(int paymentId);
+        Task<Payment?> GetPaymentForCheckoutAsync(int paymentId);
         Task<PaymentDto?> GetPaymentByAppointmentIdAsync(int appointmentId);
         Task<IEnumerable<PaymentDto>> GetAllPaymentsAsync();
         Task<IEnumerable<PaymentDto>> GetPatientPaymentsAsync(string patientId);
