46b9eef4591c26e6babca9b34daf8e7520ce5b25
Mohamed_Dawood <mboa990@gmail.com>
2026-05-14 09:18:07 +0300

    Added Drug Service

---
+++ Vezeeta/Application/Interfaces/Services/IDrugsService.cs
@@ -0,0 +1,20 @@
+﻿using Application.DTOs.Drugs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Interfaces.Services
+{
+    public interface IDrugsService
+    {
+        Task<IEnumerable<DrugDto>> GetAllAsync();
+
+        Task<DrugDto?> GetByIdAsync(int id);
+
+        Task<bool> CreateAsync(DrugDto dto);
+
+        Task<bool> UpdateAsync(int id, DrugDto dto);
+
+        Task<bool> DeleteAsync(int id);
+    }
+}
