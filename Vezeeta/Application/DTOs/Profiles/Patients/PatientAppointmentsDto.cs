17f15fbed99d3e4e325642275f533735df7ae6f3
moateff <mohamed.atef.yousef46@gmail.com>
2026-05-14 05:17:23 +0300

    Adding User Profile

---
+++ Vezeeta/Application/DTOs/Profiles/Patients/PatientAppointmentsDto.cs
@@ -0,0 +1,6 @@
+namespace Application.DTOs.Profiles.Patients
+{
+    public class PatientAppointmentsDto
+    {
+    }
+}
--- Vezeeta/Presentation/Extensions/ServiceCollectionExtensions.cs
+++ Vezeeta/Presentation/Extensions/ServiceCollectionExtensions.cs
@@ -2,6 +2,12 @@
 using Infranstructure.Persistence.Data;
 using Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Application.Interfaces.Services;
+using Application.Services;
+using Infrastructure.Services;
+using Domain.Identity;
+using Microsoft.AspNetCore.Identity;
+
 
 namespace Presentation.Extensions
 {
@@ -12,8 +18,18 @@ namespace Presentation.Extensions
             var connectionString = configuration.GetConnectionString("DefaultConnection");
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(connectionString));
+            
+            services.AddIdentity<ApplicationUser, ApplicationRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
             services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
             services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            services.AddHttpContextAccessor();
+            services.AddScoped<ICurrentUserService, CurrentUserService>();
+
+            services.AddScoped<IPatientProfileService, PatientProfileService>();
 
             return services;
         }
