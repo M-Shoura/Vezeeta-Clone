b07c000e942de7d79d74a736fb441544f4267b7e
Abutalib <abdulrahmanabutalib869@gmail.com>
2026-05-20 17:12:53 +0300

    aiintegration

---
+++ Vezeeta/Application/Interfaces/Services/IMedicalAiService.cs
@@ -0,0 +1,13 @@
+﻿using Application.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Interfaces.Services
+{
+    public interface IMedicalAiService
+    {
+        Task<string> TranscribeAudioAsync(MemoryStream audioStream);
+        Task<AiAnalysisResult> AnalyzeConsultationAsync(string transcript,string appointmentId);
+    }
+}
